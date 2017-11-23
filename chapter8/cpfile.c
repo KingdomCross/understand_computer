96ec0d2253ec86d4a1627f588acd1fcc91783e5b
WeigangZhu <Zhu_Dean@outlook.com>
2017-11-23 23:10:10 +0800

    copy a file from the stdin to stdout

---
+++ chapter8/cpfile.c
@@ -0,0 +1,20 @@
+/*
+ * cpfile.c: copy a file
+ *
+ * gcc cpfile.c csapp.c -lpthread
+ *
+ */
+
+#include"csapp.h"
+
+int main(int argc, char **argv)
+{
+	int n;
+	rio_t rio;
+	char buf[MAXLINE];
+
+	Rio_readinitb(&rio, STDIN_FILENO);
+	while(( n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
+		Rio_writen(STDOUT_FILENO, buf, n);
+}
+
