92438f6ac9881131d0d0a31d61cfc1b046584740
WeigangZhu <Zhu_Dean@outlook.com>
2018-03-30 12:24:44 +0800

    modified the PID_P parameter and adjust the line format

--- tractor/src/navigation.c
+++ tractor/src/navigation.c
@@ -4,13 +4,13 @@ double gpsYawCorrector(double gpsYaw)
 {
 	//if(gpsYaw > 270) return gpsYaw - 360;
 	//else return gpsYaw;
-	return gpsYaw;
+	return gpsYaw ;
 }
 
 
 double tarYawCreator(double endlat, double endlon, double gpslat, double gpslon)
 {
-	double t_yaw = atan2((cos(endlat)*(endlon - gpslon)),(endlat - gpslat))*180/pi;
+	double t_yaw = atan2((cos(endlat)*(endlon - gpslon)),(endlat - gpslat))*180/PI;
 	if(t_yaw < 0) t_yaw = t_yaw + 360;
 	//if(t_yaw > 90 ) t_yaw = 180 - t_yaw;
 	return t_yaw;
@@ -27,7 +27,7 @@ double distanceCal(double beglat, double beglon, double endlat, double endlon, d
 
 double distance(double a1, double b1, double a2, double b2)
 {
-	double t = ((a1-a2)*DIS_LAT_LON);
+	// double t = ((a1-a2)*DIS_LAT_LON);
 	return sqrt(pow((a1-a2)*DIS_LAT_LON, 2) + pow((b1-b2)*DIS_LAT_LON,2));
 }
 
@@ -46,28 +46,38 @@ struct CTRL_MESSAGES* navigation(struct GPS_MESSAGES* gps_msg, double endlat[], 
   t_yaw = tarYawCreator(endlat[*count], endlon[*count], gps_msg->lat, gps_msg->lon);
   yaw_now = gpsYawCorrector(gps_msg->head);
   yaw_error = t_yaw - yaw_now;
-	  
-  ctrl_msg->steer = yaw_error*(-5.0) + 90;
-  dis_error = distanceCal(endlat[*count-1], endlon[*count-1], endlat[*count], endlon[*count], gps_msg->lat, gps_msg->lon);
+  dis_error = distanceCal(endlat[*count-1], endlon[*count-1], endlat[*count], endlon[*count], gps_msg->lat, gps_msg->lon); 
+  
+  
+  ctrl_msg->steer = yaw_error*PID_P + 90;
+    
   disToend = distance(gps_msg->lat, gps_msg->lon, endlat[*count], endlon[*count]);
+  
   printf("t_yaw: %f\tnowYaw%f\tdisToend: %f\n", t_yaw,yaw_now, disToend);	
   printf("yaw_error:%f\tdis_error:%f\n", yaw_error, dis_error);
-  
+    
   if(ctrl_msg->steer < 60) ctrl_msg->steer = 60;
   if(ctrl_msg->steer > 120) ctrl_msg->steer = 120;
 	  
-  if( disToend < 3 && !enable_flag){
+  if( disToend < 3 && !enable_flag)
+  {
 		enable_flag = 1;
   }
-  if(enable_flag){ 
-	  if(fabs(gps_msg->head - endturn[*count]) < 5) {
+  
+  if(enable_flag)
+  { 
+	  if(fabs(gps_msg->head - endturn[*count]) < 5) 
+	  {
 	    ctrl_msg->steer = 90;
 		enable_flag = 0;
 	  	*count ++;
 	  }
-	  else{
-	  	if(endturn[*count] > 0) ctrl_msg->steer = 60;
-		else ctrl_msg->steer = 120;
+	  else
+	  {
+	  	if(endturn[*count] > 0) 
+	  		ctrl_msg->steer = 60;
+		else 
+			ctrl_msg->steer = 120;
       }
   }
   
