c6f5104a85050b27988ff0182a14d63021164e3e
WeigangZhu <Zhu_Dean@outlook.com>
2018-04-01 16:37:25 +0800

    modified the plot to scatter and add the x,y label

--- DBSCAN/mat_plot/plot_all.py
+++ DBSCAN/mat_plot/plot_all.py
@@ -14,6 +14,9 @@ import os
 LEN = len(os.listdir('../DBSCAN_cluster')) - 2
 PATH_pre = '../DBSCAN_cluster/cluster_'
 
+PATH_OUT = '../../line_dection/data/obj1.txt'
+fout = open(PATH_OUT,'w')
+
 for i in range(1,LEN):
 	PATH = PATH_pre + str(i) +'.txt'
 	try:
@@ -21,19 +24,24 @@ for i in range(1,LEN):
 		size_data = len(data[:,0])
 		xc = sum(data[:,0])/size_data
 		yc = sum(data[:,1])/size_data
-		# pl.plot(xc,yc,'kx', linewidth='2')
-		print(xc,yc)
-		pl.plot(data[:,0],data[:,1],'r.',linewidth='0.3')
+		pl.plot(xc,yc,'kx', linewidth='2')
+		
+		print(str(xc)  + '\t' + str(yc) + '\n')
+		fout.write(str(xc)  + '\t' + str(yc) + '\n')
+		
+		pl.scatter(data[:,0],data[:,1], s= 1, c ='r')
 	except:
 		print('Error')
 		
 
 # plot the first column as x, and second column as y
 #pl.xlim(0.0, 10.)
-
+pl.xlabel('x(m)')
+pl.ylabel('y(m)')
 pl.grid() # == plt.grid(True)
 #pl.grid(color='b' , linewidth='0.3' ,linestyle='--')
 
 pl.show()
 
+fout.close()
 
