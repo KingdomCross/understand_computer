dbcda09edd3764571eb6f717f57d191c36d781aa
WeigangZhu <Zhu_Dean@outlook.com>
2018-04-08 09:26:08 +0800

    from the many cluster files to one file

---
+++ pts_grid_cluster_hough_all_file/line_dection/cluster_to_one_file.py
@@ -0,0 +1,33 @@
+
+#coding=utf-8
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as pl
+import skimage.transform as st
+from skimage import feature
+import os
+
+PATH = '../data/cluster/004/'
+
+folders = os.listdir(PATH)
+i = 0
+for folder in folders:
+		PATH = '../data/cluster/001/'
+		OUT_PATH = '../data/cluster_point/'
+		i =  i + 1
+		if not os.path.isdir(folder):
+			PATH = PATH + folder + '/'
+			OUT_PATH = OUT_PATH + str(i) + '.txt'
+			fout = open(OUT_PATH, 'w')
+			files = os.listdir(PATH)
+			for file in files:
+				if not os.path.isdir(file):
+					data = np.loadtxt(PATH + file)
+					for (x,y) in data:
+						fout.write(str(x) + '\t' + str(y) + '\n')
+					
+					
+			
+						
+		
