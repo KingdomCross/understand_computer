a2077961b8aaec6f63d8ccf5e6725478bcb71f25
WeigangZhu <Zhu_Dean@outlook.com>
2018-04-10 20:29:02 +0800

    the status of exit

---
+++ multiprocess/pre_exit.c
@@ -0,0 +1,26 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/stat.h>
+
+
+void pre_exit(int status)
+{
+	int var;
+	pid_t pid;
+	
+	if(WIFEXITED(status))
+		printf("normal termination ,exit status = %d\n", WEXITSTATUS(status));
+	else if(WIFSIGNALED(status))
+		printf("abnormal termination, signal number = %d%s\n", WTERMSIG(status),
+#ifdef WCOREDUMP
+		WCOREDUMP(status) ? " (core file generated)":"");
+#else
+		"");
+#endif
+	
+	else if(WIFSTOPPED(status))
+		printf("child stopped, signal number = %d\n", WSTOPSIG(status));
+		
+}
+
