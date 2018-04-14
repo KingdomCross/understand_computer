5458a000bb204b78caf6262a3cf2bdd57d3dfbfe
WeigangZhu <Zhu_Dean@outlook.com>
2018-04-14 13:59:32 +0800

    add the err_sys() fucntion

---
+++ multiprocess/err_sys.c
@@ -0,0 +1,7 @@
+#include"err_sys.h"
+
+void err_sys(const char *msg)
+{
+	printf("%s\n", msg);
+	exit(1);
+}
---
+++ multiprocess/err_sys.h
@@ -0,0 +1,10 @@
+#ifndef ERR_SYS_H_
+#define ERR_SYS_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void err_sys(const char*);
+
+
+#endif
