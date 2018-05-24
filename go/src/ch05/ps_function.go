2e3b0508746d8ce37bd8c2b49d4753cffd1adee1
WeigangZhu <Zhu_Dean@outlook.com>
2018-05-24 15:23:50 +0800

    the usage of function

---
+++ go/src/ch05/ps_function.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(x int, y int) int { return x + y }
+
+func sub(x, y int) (z int) { z = x - y; return }
+
+func first(x int, _ int) int { return x }
+
+func zero(int, int) int { return 0 }
+
+func ps_type() {
+	fmt.Printf("%T\n", add)
+	fmt.Printf("%T\n", sub)
+	fmt.Printf("%T\n", first)
+	fmt.Printf("%T\n", zero)
+}
+
+func main() {
+	ps_type()
+}
