6131ced425027ece8a2e25f905e58ec3d04cdb31
WeigangZhu <Zhu_Dean@outlook.com>
2018-06-11 15:26:08 +0800

    the model of 'path error'

---
+++ go/src/ch07/path_error.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+// PathError records an error and the operation and file path that caused it
+type PathError struct {
+	Op   string
+	Path string
+	Err  error
+}
+
+func (e *PathError) Error() string {
+	return e.Op + " " + e.Path + ": " + e.Error()
+}
+
+
+func main() {
+	_, err := os.Open("no/such/file")
+	fmt.Println(err)
+	fmt.Printf("%#v\n", err)
+}
