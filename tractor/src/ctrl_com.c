0712deb1ecc56b7c23b0022363142c5c4fd2d4c0
WeigangZhu <Zhu_Dean@outlook.com>
2018-07-18 22:39:44 +0800

    move all the files to the paper folder

--- tractor/src/ctrl_com.c
+++
@@ -1,9 +0,0 @@
-#include"ctrl_com.h"
-
-void ctrl_send(unsigned char se_buf[], struct CTRL_MESSAGES* send, int fd_ctrl, int len ){
-	se_buf[0] = send->steer;
-	write(fd_ctrl, se_buf, len);
-	printf("speed:%d\tsteer:%d\tpower:%d\n", send->speed, send->steer, send->power);  
-}
-
-
