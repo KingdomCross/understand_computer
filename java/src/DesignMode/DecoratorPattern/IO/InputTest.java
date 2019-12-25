e1f1c03cdd62df643f87db7505b95588f7959ecb
WeigangZhu <Zhu_Dean@outlook.com>
2019-12-25 21:58:36 +0800

    decorator pattern of IO

---
+++ java/src/DesignMode/DecoratorPattern/IO/InputTest.java
@@ -0,0 +1,22 @@
+package DesignMode.DecoratorPattern.IO;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class InputTest {
+    public static void main(String[] args) throws IOException {
+        int c;
+        try {
+            LowerCaseInputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("/home/dean/.gitconfig")));
+
+            while((c = in.read())> 0) {
+                System.out.print((char)c);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
